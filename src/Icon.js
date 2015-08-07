6144c5d8481ed756dd5791826a44a340e535aa67
Joss Mackison <joss.mackison@gmail.com>
2015-08-07 17:41:55 +1000

    linter fixes

--- src/Icon.js
+++ src/Icon.js
@@ -1,10 +1,10 @@
-import blacklist from 'blacklist';
+// import blacklist from 'blacklist';
 import { createClass, createElement, PropTypes } from 'react';
 // import ionicons from './ionicons-map';
 
 const ionicons = {
-	'test': '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="512px" height="512px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"><path d="M476.7,422.2L270.1,72.7c-2.9-5-8.3-8.7-14.1-8.7c-5.9,0-11.3,3.7-14.1,8.7L35.3,422.2c-2.8,5-4.8,13-1.9,17.9 c2.9,4.9,8.2,7.9,14,7.9h417.1c5.8,0,11.1-3,14-7.9C481.5,435.2,479.5,427.1,476.7,422.2z M288,400h-64v-48h64V400z M288,320h-64 V176h64V320z"/></svg>',
-}
+	'test': '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="512px" height="512px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"><path d="M476.7,422.2L270.1,72.7c-2.9-5-8.3-8.7-14.1-8.7c-5.9,0-11.3,3.7-14.1,8.7L35.3,422.2c-2.8,5-4.8,13-1.9,17.9 c2.9,4.9,8.2,7.9,14,7.9h417.1c5.8,0,11.1-3,14-7.9C481.5,435.2,479.5,427.1,476.7,422.2z M288,400h-64v-48h64V400z M288,320h-64 V176h64V320z"/></svg>'
+};
 const CLEANUPS = {
 	// some useless junk
 	title: /<title>.*<\/title>/gi,
@@ -22,8 +22,8 @@ const CLEANUPS = {
 	// Sketch.app shit
 	sketchMSShapeGroup: / +sketch:type=\"MSShapeGroup\"/gi,
 	sketchMSPage: / +sketch:type=\"MSPage\"/gi,
-	sketchMSLayerGroup: / +sketch:type=\"MSLayerGroup\"/gi,
-}
+	sketchMSLayerGroup: / +sketch:type=\"MSLayerGroup\"/gi
+};
 
 var Icon = createClass({
 	displayName: 'Icon',
@@ -31,22 +31,22 @@ var Icon = createClass({
 	propTypes: {
 		color: PropTypes.string,
 		name: PropTypes.oneOf(Object.keys(ionicons)).isRequired,
-		size: PropTypes.number,
+		size: PropTypes.number
 	},
 	
 	getDefaultProps () {
 		return {
 			color: 'black',
 			size: 32,
-		}
+		};
 	},
 
 	cleanupSvg(svg) {
 		return Object.keys(CLEANUPS)
 			.reduce((acc, key) => {
-				return acc.replace(CLEANUPS[key], "")
+				return acc.replace(CLEANUPS[key], '');
 			}, svg)
-			.trim()
+			.trim();
 	},
 
 	render () {
@@ -56,9 +56,6 @@ var Icon = createClass({
 		let svgClasses = 'SVGIcon ' + this.props.className;
 		
 		height = width = this.props.size;
-
-		console.log(fill);
-		let props = blacklist(this.props, { name: null, color: null, size: null });
 		
 		return createElement('div', {
 			style: {
